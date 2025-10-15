74c5314a30aeb4ea4004d3ec7b53e14370b17132
joojoo <jyh06151@phidigital.co.kr>
2025-10-15 11:27:44 +0900

    chore: docker 및 mongo init 설정

--- app/mongo-init.js
+++ app/mongo-init.js
@@ -1,4 +1,7 @@
-// init-mongo.js
+db = db.getSiblingDB('admin');
+db.system.users.update({"user":"init"}, {$set:{"user":"mongo_root"}})
+db.changeUserPassword('mongo_root', 'mongoDkagh#2')
+
 db = db.getSiblingDB("skeleton"); // skeleton DB 선택/생성
 
 db.createUser({
@@ -8,5 +11,10 @@ db.createUser({
         { role: "readWrite", db: "skeleton" }
     ]
 });
+
+// log quiet
+db.setLogLevel(0);
+// install after retry...
+disableTelemetry();
 
 db.createCollection("user");
